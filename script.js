d6fce153c301ef29b71d2b16e29ca26d06f8c8f4
AlvaroAbreu <alvaroabreu1509@gmail.com>
2022-08-31 22:00:49 -0300

    estilizando o requisito 4 para melhor visualização

--- script.js
+++ script.js
@@ -6,17 +6,14 @@
        let bloco3 = document.querySelector('#color3');
        let bloco4 = document.querySelector('#color4');
        
-       let randomRgb1 = Math.floor(Math.random() * 255);
-       let randomRgb2 = Math.floor(Math.random() * 255);
-       let randomRgb3 = Math.floor(Math.random() * 255);
-       let randomRgb4 = Math.floor(Math.random() * 255);
-       let randomRgb5 = Math.floor(Math.random() * 255);
-       let randomRgb6 = Math.floor(Math.random() * 255);
-       let randomRgb7 = Math.floor(Math.random() * 255);
-       let randomRgb8 = Math.floor(Math.random() * 255);
-       let randomRgb9 = Math.floor(Math.random() * 255);
-       bloco2.style.backgroundColor = 'rgb(' + randomRgb1 + ', ' + randomRgb2 + ', ' + randomRgb3 + ')';
-       bloco3.style.backgroundColor = 'rgb(' + randomRgb4 + ', ' + randomRgb5 + ', ' + randomRgb6 + ')';
-       bloco4.style.backgroundColor = 'rgb(' + randomRgb7 + ', ' + randomRgb8 + ', ' + randomRgb9 + ')';
+        let corBloco2 = bloco2.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+        let corBloco3 = bloco3.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+        let corBloco4 = bloco4.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+       
+
+        
     })
    }
+
+
+   
