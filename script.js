948e48fb2c89aeb30627694428fd4ac719fcc939
AlvaroAbreu <alvaroabreu1509@gmail.com>
2022-09-01 18:08:30 -0300

    requisitos 6, 7 e 8 terminados

--- script.js
+++ script.js
@@ -1,19 +1,64 @@
-   window.onload = function() {
+ window.onload = function() {
     let button = document.getElementById('button-random-color');
+    
+  
     button.addEventListener('click', function(){
 
-       let bloco2 = document.querySelector('#color2');
-       let bloco3 = document.querySelector('#color3');
-       let bloco4 = document.querySelector('#color4');
+       const bloco2 = document.querySelector('#color2');
+       const bloco3 = document.querySelector('#color3');
+       const bloco4 = document.querySelector('#color4');
        
-        let corBloco2 = bloco2.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
-        let corBloco3 = bloco3.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
-        let corBloco4 = bloco4.style.backgroundColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+        let corBloco2 = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+        bloco2.style.backgroundColor = corBloco2;
+
+        let corBloco3 = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+
+        bloco3.style.backgroundColor = corBloco3;
+
+        let corBloco4 = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
+        bloco4.style.backgroundColor = corBloco4;
+
        
+      const palette = [corBloco2, corBloco3, corBloco4];
+      const storage = localStorage;
+      storage.setItem('colorPalette', JSON.stringify(palette));
+      
+      
+ })
+ /*function recovered () {
+   let recoveredArray = JSON.parse(localStorage.getItem('colorPalette'));
+   cor1 = recoveredArray[0];
+   cor2 = recoveredArray[1];
+   cor3 = recoveredArray[2];
+   document.querySelector('#color2').style.backgroundColor = cor1;
+   document.querySelector('#color3').style.backgroundColor = cor2;
+   document.querySelector('#color4').style.backgroundColor = cor3;
+ }
+   recovered();
+   */
+}
 
-        
-    })
-   }
 
+ let elementoFilho = document.querySelector('#color-palette').children;
+ console.log(elementoFilho);
+ let elemento = document.getElementById('color2');
+  elemento.addEventListener('click', function() {  
+   
 
    
+    
+      if (elementoFilho[1].classList = 'color') {
+         elementoFilho[1].classList.add('selected')
+      } 
+        
+   
+
+    });
+    
+ 
+
+
+ 
+
+
+
